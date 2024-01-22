d7e0a7e9bfd18bcdf322eeff21110b7f077ca639
´´George <george.emannuelcarvalho@gmail.com>
2024-01-22 13:27:32 -0300

    apps.js

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 6
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número secreto entre 1 e 10');
+let chute 
+
+// enquanto o chute não for igual ao número secreto //
+
+while (chute != numeroSecreto) {
+  chute = prompt('Escolha um número entre 1 e 10');
+}
 
 // se for igual ao numero secreto //
 
 if (chute == numeroSecreto){
   alert(`Isso ai! você descobriu o número secretro ${numeroSecreto}`);
 }else{
-  alert('você errou :(')
+
+  if(chute > numeroSecreto){
+    alert(`O número secreto é menor que ${chute}`);
+
+  } else{
+    alert(`O número secreto é maior que ${chute}`)
+  }
 }
